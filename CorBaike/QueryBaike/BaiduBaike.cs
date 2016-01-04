a52d9335c41b144082adcbcb3ee4fda086ed2ae2
Emeryao <Emeryao@users.noreply.github.com>
2016-01-04 13:44:36 +0800

    change a baidu get url and use wap html

--- CorBaike/QueryBaike/BaiduBaike.cs
+++ CorBaike/QueryBaike/BaiduBaike.cs
@@ -13,24 +13,28 @@ namespace QueryBaike
             string content = "";
             string keyWord = System.Net.WebUtility.UrlEncode(keyword);
 
-            content = await GetHttpResponse($"http://baike.baidu.com/search?word={keyWord}&pn=0&rn=0&enc=utf8");
-
-            if (content.Contains("没有找到"))
+            //content = await GetHttpResponse($"http://baike.baidu.com/search?word={keyWord}&pn=0&rn=0&enc=utf8");
+            content = await GetHttpResponse($"http://baike.baidu.com/search/word?word={keyWord}",true);
+            
+            if (content.Contains("没有找到")||content.Contains("search/none"))
             {
                 return $"抱歉，没有找到与{keyword}相关的百科结果。";
             }
+            
+            // replace to wap baike to shorter the html
+            content = content.Replace("baike","wapbaike");
+            
+            //HtmlParser parser = new HtmlParser();
+            //Document doc = parser.Parse(content);
 
-            HtmlParser parser = new HtmlParser();
-            Document doc = parser.Parse(content);
-
-            var tag = doc.Find<HtmlSharp.Elements.Tags.A>("dl.search-list a.result-title");
-            if (!string.IsNullOrWhiteSpace(tag?.Href))
+            //var tag = doc.Find<HtmlSharp.Elements.Tags.A>("dl.search-list a.result-title");
+            if (!string.IsNullOrWhiteSpace(content))
             {
-                content = await GetHttpResponse(tag?.Href);
+                content = await GetHttpResponse(content);
 
                 Document detailDoc = parser.Parse(content);
 
-                var div = detailDoc.Find<HtmlSharp.Elements.Tags.Div>("div.lemma-summary div.para");
+                var div = detailDoc.Find<HtmlSharp.Elements.Tags.P>(".summary+p");
 
                 string strText = System.Text.RegularExpressions.Regex.Replace(div.ToString(), "<[^>]+>", "");
                 strText = System.Text.RegularExpressions.Regex.Replace(strText, "&[^;]+;", "");
@@ -42,12 +46,15 @@ namespace QueryBaike
         }
 
 
-        private async static Task<string> GetHttpResponse(string url)
+        private async static Task<string> GetHttpResponse(string url,bool uriOnly=false)
         {
             HttpWebRequest request = WebRequest.CreateHttp(url);
 
             WebResponse response = await request.GetResponseAsync();
-
+            
+            if(uriOnly)
+                return response.ResponseUri?.AbsoluteUri;
+            
             Stream s = response.GetResponseStream();
 
             using (StreamReader sr = new StreamReader(s))
