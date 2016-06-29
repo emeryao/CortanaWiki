433fbff6223d13ab08a8652a17a83da67d46de3d
Emeryao <luyao1206@live.cn>
2016-06-29 11:52:39 +0800

    release v1.6.0

--- CorBaike/QueryBaike/BaiduBaike.cs
+++ CorBaike/QueryBaike/BaiduBaike.cs
@@ -1,6 +1,8 @@
-﻿using HtmlSharp;
+﻿using AngleSharp.Parser.Html;
+using Newtonsoft.Json;
 using System;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -26,45 +28,63 @@ namespace QueryBaike
 
             string keyWord = System.Net.WebUtility.UrlEncode(keyword);
 
-            string url = await GetHttpResponse($"http://baike.baidu.com/search/word?word={keyWord}", true);
+            string searchResult = await GetHttpResponse($"http://baike.baidu.com/client/search?word={keyWord}");
 
-            if (url.Contains("没有找到") || url.Contains("search/none"))
+            var jResult = (Newtonsoft.Json.Linq.JObject)JsonConvert.DeserializeObject(searchResult);
+            string id = jResult["lemmaId"]?.ToString();
+
+
+            if (string.IsNullOrWhiteSpace(id))
             {
                 retData.Summary = $"抱歉，没有找到与{keyword}相关的百科结果。";
                 return retData;
             }
+            
+            string url = $"http://wapbaike.baidu.com/view/{id}.htm";
 
-            // replace to wap baike to shorter the html
-            url = url.Replace("baike", "wapbaike");
+            //string clientUrl = $"http://baike.baidu.com/client/view/{id}.htm";
+
             retData.Url = url;
             if (!string.IsNullOrWhiteSpace(url))
             {
                 string content = await GetHttpResponse(url);
 
                 HtmlParser parser = new HtmlParser();
-                Document detailDoc = parser.Parse(content);
 
-                var para = detailDoc.Find<HtmlSharp.Elements.Tags.P>(".summary+p");
+                var detailDoc = parser.Parse(content);
 
-                if (para == null)
-                    para = detailDoc.Find<HtmlSharp.Elements.Tags.P>(".summary>p");
+                var elements = detailDoc.QuerySelectorAll("div#main>div.card>p.summary~p");
 
-                if (para != null)
+                if (elements?.Length > 0)
                 {
-                    string strText = System.Text.RegularExpressions.Regex.Replace(para.ToString(), "<[^>]+>", "");
-                    strText = System.Text.RegularExpressions.Regex.Replace(strText, "&[^;]+;", "");
-                    strText = System.Text.RegularExpressions.Regex.Replace(strText, "\\[[^>]+\\]", "");
+                    string strText = "";
+                    elements.ToList().ForEach((e) =>
+                    {
+                        strText += e.TextContent;
+                    });
+
+                    strText = strText.Replace("\n", "").Replace("\n", "").Replace(" ", "");
+
+                    if (strText.Length > 230)
+                    {
+                        strText = strText.Remove(230);
+                        if (strText.Contains("。"))
+                            strText = strText.Remove(strText.LastIndexOf("。"));
+                    }
+
                     retData.Summary = strText + "\r\n转到小娜百科就可以查看详细的百科信息了哦！么么哒！";
 
-                    var img = detailDoc.Find<HtmlSharp.Elements.Tags.Img>(".img-box>a>img");
+                    var img = detailDoc.QuerySelector(".img-box>a>img");
 
-                    if (!string.IsNullOrWhiteSpace(img?.Src))
+                    string imageSource = img?.Attributes["src"].Value;
+
+                    if (!string.IsNullOrWhiteSpace(imageSource))
                     {
 
                         var file = await ApplicationData.Current.TemporaryFolder.CreateFileAsync($"{keyword}.jpg", CreationCollisionOption.OpenIfExists);
                         using (HttpClient client = new HttpClient())
                         {
-                            var bytes = await client.GetByteArrayAsync(img.Src);
+                            var bytes = await client.GetByteArrayAsync(imageSource);
 
                             await FileIO.WriteBytesAsync(file, bytes);
                         }
@@ -88,15 +108,12 @@ namespace QueryBaike
         }
 
 
-        private async static Task<string> GetHttpResponse(string url, bool uriOnly = false)
+        private async static Task<string> GetHttpResponse(string url)
         {
             HttpWebRequest request = WebRequest.CreateHttp(url);
 
             using (WebResponse response = await request.GetResponseAsync())
             {
-                if (uriOnly)
-                    return response.ResponseUri?.AbsoluteUri;
-
                 Stream s = response.GetResponseStream();
 
                 using (StreamReader sr = new StreamReader(s))
